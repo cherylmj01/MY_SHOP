65ffeb3d3cf6f3f711b9ef701fb4b5b4caf529ea
cherylmj01 <cherylmj01@gmail.com>
2024-06-17 10:48:47 -0600

    Updated Code with navigation link and routes

--- myshop/src/App.js
+++ myshop/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route} from 'react-router-dom';
 
-function App() {
+import Home from './components/routes/home/home.component';
+import Shop from "./components/routes/shop/shop.component";
+import Navigation from "./components/routes/navigation/navigation.component"; 
+import SignIn from "./components/routes/sign-in/sign-in.component";
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <Routes>
+    <Route path='/' element={<Navigation />}>
+      <Route index element={<Home />} />
+      <Route path='shop' element={<Shop />} />
+      <Route path='sign-in' element={<SignIn />} />
+    </Route> 
+  </Routes>
+)
 }
 
 export default App;
